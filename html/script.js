86f224d25f98e3e5cc6a568d842a27ff1ae629ec
ouzkagan <ogzkgnlmz2@gmail.com>
2021-11-23 03:36:03 +0300

    yatis too many changes damn

--- html/script.js
+++ html/script.js
@@ -1,9 +1,9 @@
-let generationNumber = 600
+let generationNumber = null
 let allFiles = []
 let directories = [];
 let directoryCounts = {}
 let directoryList = {}
-let order = ['background', 'ball', 'eye color', 'iris', 'top lid', 'bottom lid', 'shine']  //use directories for now ** later it will be user decided
+let order = []  //use directories for now ** later it will be user decided
 let downloadQuery = []
 var inps = document.querySelectorAll("input");
 [].forEach.call(inps, function (iptEl) {
@@ -24,50 +24,168 @@ var inps = document.querySelectorAll("input");
                 directoryList[directory] = [this.files[i]]
 
             }
+            if(i == this.files.length -1 ){
+                generateDirectoryOrder(directories)
+            }
         }
         // console.log(directoryList)
+        document.querySelector('.steps').style.display='none'
+        document.querySelector('.reorder').style.display='block'
 
         
     };
 });
+
+const subdirectories = document.querySelector('.sub-directories')
+function generateDirectoryOrder(directories) {
+/* <li class="directory">/Layer 0</li>
+            <li class="directory">/Layer 1</li>  */
+            order = []
+    subdirectories.innerHTML = directories.map(item => {
+        order.push(item)
+        return `<li class="directory" data-directory="${item}">/${item}</li>`
+    }).join('')
+    console.log(order)
+}
+
+
+function notify(a){
+    console.log(a)
+}
+const obj = {
+    async drawImages(ctx, images) {
+      // return a Promise synchronously
+      return new Promise((resolve, reject) => {
+        let img = images.shift();
+        // console.log(images.length)
+
+        this.imgToDraw = new Image();
+        this.imgToDraw.src = img;
+        this.imgToDraw.onload = () => {
+          ctx.drawImage(this.imgToDraw, 0, 0); // Draws the image on canvas
+          if (images.length > 0) {
+              //   console.log(images)
+              // resolve with next iteration so we can await all
+            notify(images.length); 
+            resolve(this.drawImages(ctx, images));
+          } else {
+            console.log("I - Processing");
+            // done
+            var hiddenCanvas = document.getElementById("hiddenCanvas");
+            let imgData = hiddenCanvas.toDataURL("image/png"); // Assigns image base64 string in PNG format to a variable
+            // console.log(imgData)
+            downloadQuery.push(imgData)
+            resolve();
+          }
+        };
+        this.imgToDraw.onerror = reject;
+      });
+    }
+  };
+let imagesToDraw = []
+  
+
+const uploadDocuments = async (event, files) => {
+    const filePromises = files.map((file) => {
+      // Return a promise per file
+      return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = async (e) => {
+          try {
+            fileContent = reader.result;
+
+            // Resolve the promise with the response value
+            resolve(fileContent);
+          } catch (err) {
+            reject(err);
+          }
+        };
+        reader.onerror = (error) => {
+          reject(error);
+        };
+      });
+    });
+  
+    // Wait for all promises to be resolved
+    const fileInfos = await Promise.all(filePromises);
+  
+    console.log('file infos COMPLETED');
+  
+    // Profit
+    return fileInfos;
+  };
+
 //imageFiles: bottom to top
-function putImagesToCanvas(imageFiles) {
+const putImagesToCanvas = async (imageFiles,cb) => {
     var hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
     var ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
     ctx.clearRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
-    for (let i = 0; i < imageFiles.length; i++) {
-        var reader = new FileReader();
+    const imagesToDraw = await uploadDocuments(0, imageFiles)
 
-        if (imageFiles[i]) {
+    // console.log(imagesToDraw)
+    // cb(imagesToDraw);
+    // for (let i = 0; i < imageFiles.length; i++) {
+    //     let reader = new FileReader();
+
+    //     if (imageFiles[i]) {
+    //         // console.log(imageFiles[i])
+    //         reader.readAsDataURL(imageFiles[i]);
+    //     }
+    //     else { return }
+    //     // reader.readAsDataURL(imageFiles[i]);
+    //     reader.onloadend = function (e) {
+    //         let gImage = new Image(); // Creates image object
+    //         gImage.src = e.target.result; // Assigns converted image to image object
+    //         // gImage.posIndex = i;
+    //         gImage.onload = function (ev) {
+    //             if (i == 0) {
+    //                 imagesToDraw = []
+    //                 hiddenCanvas.width = gImage.width; // Assigns image's width to canvas
+    //                 hiddenCanvas.height = gImage.height; // Assigns image's height to canvas
+    //                 // console.log("=====================================================\n")
+    //             }
+    //             imagesToDraw.push(e.target.result)
+    //             // console.log(imageFiles[i].webkitRelativePath, order[i])
+    //             // ctx.drawImage(this, 0, 0); // Draws the image on canvas
+    //             if(i === imageFiles.length - 1) {
+    //                 cb(imagesToDraw);
+    //                     // let imgData = hiddenCanvas.toDataURL("image/png"); // Assigns image base64 string in PNG format to a variable
+    //                     // // console.log(imgData)
+    //                     // downloadQuery.push(imgData)
+    //                 }
+    //             };
+    //     };
+    // }
+         let reader = new FileReader();
+        if (imageFiles[0]) {
             // console.log(imageFiles[i])
-            reader.readAsDataURL(imageFiles[i]);
+            reader.readAsDataURL(imageFiles[0]);
         }
         else { return }
         // reader.readAsDataURL(imageFiles[i]);
         reader.onloadend = function (e) {
             let gImage = new Image(); // Creates image object
             gImage.src = e.target.result; // Assigns converted image to image object
-            gImage.posIndex = i;
-
+            // gImage.posIndex = i;
             gImage.onload = function (ev) {
-                if (i == 0) {
                     hiddenCanvas.width = gImage.width; // Assigns image's width to canvas
                     hiddenCanvas.height = gImage.height; // Assigns image's height to canvas
-                    // console.log("=====================================================\n")
-                }
-                // console.log(imageFiles[i].webkitRelativePath, order[i])
-                ctx.drawImage(this, 0, 0); // Draws the image on canvas
-                if(i === imageFiles.length - 1) {
-                    let imgData = hiddenCanvas.toDataURL("image/png"); // Assigns image base64 string in PNG format to a variable
-                    // console.log(imgData)
-                    downloadQuery.push(imgData)
-                }
-            };
+                    cb(imagesToDraw);
+                };
         };
-    }
+
 
 }
-
+    // draw images asyncli
+const asdfg = async (imagesToDraw) => {
+    // console.log(imagesToDraw)
+    let hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
+    let ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
+    await obj.drawImages(ctx, imagesToDraw);
+    // fill a green rect over to show we are able to await it
+    console.log("all done");
+    }
 //return combinations()
 // var a = [
 //     [1, 2,3,4],
@@ -97,10 +215,11 @@ function ggenerator() {
     // create 2xdimensional array
     let numbers = [
     ]
+    let revOrder = [...order].reverse()
     for (let i = 0; i < Object.keys(directoryCounts).length; i++) {
-        numbers.push([...Array.from(Array(directoryCounts[order[i]]).keys())])
+        numbers.push([...Array.from(Array(directoryCounts[revOrder[i]]).keys())])
     }
-    console.log(numbers)
+    // console.log(numbers)
     //allPossibleCombinations gives last out of index. that's why i put this.
     // numbers.push([0])
 
@@ -118,36 +237,46 @@ function ggenerator() {
     let generateArray = []
     let row = []
     // let generateArray = allPossibleCombinations(numbers)[0].map
+    generationNumber = Number(document.querySelector('.limit-number').value) || 9999999999999999999999999999999
     let finalGenerationNumber = generationNumber > combinations.length ? combinations.length : generationNumber
-    console.log(generationNumber, combinations.length,finalGenerationNumber )
+    // console.log(generationNumber, combinations.length,finalGenerationNumber )
+    // console.log(order)
+    revOrder = [...order].reverse()
     for (let i = 0; i < finalGenerationNumber; i++) {
         row = []
-        for (let j = 0; j < order.length; j++) {
-
-            if(directoryList[order[j]][combinations[i][j]] ===undefined) {
-                console.log(order[j], combinations[i])
-                console.log(directoryList[order[j]],[combinations[i][j]])
-            }
-            row.push(directoryList[order[j]][combinations[i][j]])
+        for (let j = 0; j < revOrder.length; j++) {
+            // console.log(directoryList[revOrder[j]],combinations[i],[combinations[i][j]])
+            row.push(directoryList[revOrder[j]][combinations[i][j]])
         }
-        console.log(row)
         generateArray.push(row)
     }
 
     // console.log(generateArray)
-    for (let i = 0; i < finalGenerationNumber; i++) {
-        putImagesToCanvas([...generateArray[i]])
+    let k = 0;
+    for (k = 0; k < finalGenerationNumber; k++) {
+        putImagesToCanvas([...generateArray[k]],asdfg)
+        if(k === finalGenerationNumber -1){
+            downloadButton.disabled = false
+            document.querySelector('.completed').style.display = "block"
+        }
     }
+    
 }
 const generateButton = document.querySelector(".generate-btn"); 
 const downloadButton = document.querySelector(".download-button"); 
 downloadButton.addEventListener('click', (e)=>{
     queryDownloader([...downloadQuery])
+    downloadQuery=[]
 })
 
 generateButton.addEventListener('click', (e)=>{
+    downloadQuery=[]
     //check if directories are done
-    ggenerator()
+    if(directories.length !== 0){ 
+        ggenerator();
+        
+    }
+    
 })
 
 
@@ -166,4 +295,21 @@ function queryDownloader(dataURIs) {
             // see FileSaver.js
             saveAs(content, "artwork.zip");
         });
+}
+
+
+// Initialize Dragula
+const drake = dragula([document.querySelector('.sub-directories')]);
+
+drake.on('drop',function(el,target,source,sibling){
+    reorderDirectories()
+})
+    
+// follow sub directories
+function reorderDirectories(){
+    const newOrder = []
+    subdirectories.childNodes.forEach(item => {
+        newOrder.push(item.getAttribute('data-directory'))
+    })
+    order = [...newOrder]
 }
