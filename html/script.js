0e3826b4039d60ae414d11fc6ab9835dd9270d79
ouzkagan <ogzkgnlmz2@gmail.com>
2021-11-23 15:24:15 +0300

    working correctly

--- html/script.js
+++ html/script.js
@@ -5,6 +5,7 @@ let directoryCounts = {}
 let directoryList = {}
 let order = []  //use directories for now ** later it will be user decided
 let downloadQuery = []
+let finalGenerationNumber = 0
 var inps = document.querySelectorAll("input");
 [].forEach.call(inps, function (iptEl) {
     iptEl.onchange = function (e) {
@@ -54,6 +55,8 @@ function notify(a) {
 }
 const obj = {
     async drawImages(ctx, images) {
+
+
         // return a Promise synchronously
         return new Promise((resolve, reject) => {
             let img = images.shift();
@@ -85,6 +88,100 @@ const obj = {
 let imagesToDraw = []
 
 
+//right now we try to draw on same canvas adnd we are calling this function in sync loop. so it doesn't wait for it to finish.
+//maybe we can fix this via creating different canvas for each array.
+//instead of ctx we can send i and that would hold the information about sequence
+
+const promiseOfAllImages = async (tiles) => {
+    // console.log(tiles)
+    // Wait until ALL images are loaded
+    return Promise.all(
+        tiles.map(function (t) {
+            // Load each tile, and "resolve" when done
+            return new Promise(async (resolve) => {
+                const img = new Image();
+                img.src = t;
+                img.onload = function () {
+                    // Image has loaded... resolve the promise!
+                    resolve(img);
+                };
+            });
+        })
+    );
+};
+const promiseOfAllCanvas = function (sequence, images) {
+    console.log(images)
+    return Promise.all(
+        images.map(function (image, index) {
+            // Load each tile, and "resolve" when done
+            return new Promise(function (resolve) {
+                if (document.getElementById(`canvas-id-${sequence}`) === null) {
+                    document.querySelector('.canvas-container').innerHTML += `<canvas class="generated-canvas" style="width: 75%;" id='canvas-id-${sequence}'>`;
+                }
+                let disposableCanvas = document.getElementById(`canvas-id-${sequence}`);
+                if(index === 0){
+                    disposableCanvas.width = "512"
+                    disposableCanvas.height = "512"
+                }
+                let ctx = disposableCanvas.getContext("2d");
+
+                console.log(`drawing seq-${sequence} ${image}`)
+                ctx.drawImage(image, 0, 0)
+                resolve(disposableCanvas.toDataURL("image/png"))
+            });
+        })
+    );
+}
+
+const drawImages = async (sequence, images) => {
+
+    // return a Promise synchronously
+    return new Promise((resolve, reject) => {
+        if (document.getElementById(`canvas-id-${sequence}`) === null) {
+            document.querySelector('.canvas-container').innerHTML += `<canvas style="width: 75%;" id='canvas-id-${sequence}'>`;
+        }
+        let disposableCanvas = document.getElementById(`canvas-id-${sequence}`);
+        let ctx = disposableCanvas.getContext("2d");
+
+        if (images.length === order.length) {
+            console.log('first image--take size. seq number:', sequence)
+            let gImage = new Image(); // Creates image object
+            gImage.src = images[0]; // Assigns converted image to image object
+            gImage.onload = () => {
+                console.log("setted size of to", disposableCanvas, gImage.width, gImage.height)
+                disposableCanvas.width = gImage.width; // Assigns image's width to canvas
+                disposableCanvas.height = gImage.height; // Assigns image's width to canvas
+            }
+        }
+
+        let img = images.shift();
+        // console.log(images.length)
+
+        imgToDraw = new Image();
+        imgToDraw.src = img;
+        imgToDraw.onload = () => {
+            ctx.drawImage(imgToDraw, 0, 0); // Draws the image on canvas
+            console.log(`Image number ${order.length - images.length} drawed on canvas no canvas-id-${sequence}.`)
+            if (images.length > 0) {
+                //   console.log(images)
+                // resolve with next iteration so we can await all
+                notify(images.length);
+                resolve(drawImages(sequence, images));
+            } else {
+                console.log(`seq number ${sequence} is finishing`);
+                // done
+                let imgData = disposableCanvas.toDataURL("image/png"); // Assigns image base64 string in PNG format to a variable
+                // console.log(imgData)
+                downloadQuery.push(imgData)
+                resolve(disposableCanvas.remove());
+
+            }
+        };
+        this.imgToDraw.onerror = reject;
+    });
+}
+
+
 const uploadDocuments = async (event, files) => {
     const filePromises = files.map((file) => {
         // Return a promise per file
@@ -94,9 +191,11 @@ const uploadDocuments = async (event, files) => {
             reader.onloadend = async (e) => {
                 try {
                     fileContent = reader.result;
-
+                    
+                    // console.log(file.webkitRelativePath)
                     // Resolve the promise with the response value
                     resolve(fileContent);
+                    //resolve({img:fileContent, name:file.webkitRelativePath})
                 } catch (err) {
                     reject(err);
                 }
@@ -117,38 +216,93 @@ const uploadDocuments = async (event, files) => {
 };
 
 //imageFiles: bottom to top
-const putImagesToCanvas = async (imageFiles, cb) => {
-    var hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
-    var ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
-    ctx.clearRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
-    const imagesToDraw = await uploadDocuments(0, imageFiles)
+const putImagesToCanvas = async (imageFiles, cb, seq) => {
+    // var hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
+    // var ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
+    // ctx.clearRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
+    const imagesToDraw = await uploadDocuments(0, imageFiles)//correctly gives the sequence for every image
+    // console.log(imagesToDraw)
 
-    let reader = new FileReader();
-    if (imageFiles[0]) {
-        // console.log(imageFiles[i])
-        reader.readAsDataURL(imageFiles[0]);
-    }
-    else { return }
-    // reader.readAsDataURL(imageFiles[i]);
-    reader.onloadend = function (e) {
-        let gImage = new Image(); // Creates image object
-        gImage.src = e.target.result; // Assigns converted image to image object
-        // gImage.posIndex = i;
-        gImage.onload = function (ev) {
-            hiddenCanvas.width = gImage.width; // Assigns image's width to canvas
-            hiddenCanvas.height = gImage.height; // Assigns image's height to canvas
-            cb(imagesToDraw);
-        };
-    };
+    document.querySelector('.completed').style.display = "block"
+    document.querySelector('.completed').innerText = "Preparing. " + seq 
+    const downloadableContent = await promiseOfAllImages(imagesToDraw)
+        .then(function (allImages) {
+            // console.log("All images are loaded!", allImages); // [Img, Img, Img]
+            // draw(tiles, allImages, ground);
+            return allImages
+        }).then(all => {
+
+            // console.log("drawing images to canvas started for seq-",seq)
+            return promiseOfAllCanvas(seq, all)
+        }).then(x=>{
+
+            // console.log("adding to download query. download query length:", downloadQuery.length)
+            downloadQuery.push( x[order.length-1])
+            return downloadQuery.length //last state of canvas
+        }).then(done => {
+            setTimeout(() => {
+                document.getElementById(`canvas-id-${seq}`).remove()
+            }, 250);
+            if(done === finalGenerationNumber){
+                downloadButton.disabled = false
+                downloadButton.style.cursor = 'pointer';
+                document.querySelector('.completed').innerText = "Completed"
+                
+            }
+        })
+        .catch(err => console.log(err))
+    // await asdfg(imagesToDraw, seq);
+    
+        // console.log(downloadableContent)
+    // let reader = new FileReader();
+    // if (imageFiles[0]) {
+    //     // console.log(imageFiles[i])
+    //     reader.readAsDataURL(imageFiles[0]);
+    // }
+    // else { return }
+    // // reader.readAsDataURL(imageFiles[i]);
+    // reader.onloadend = function (e) {
+    //     let gImage = new Image(); // Creates image object
+    //     gImage.src = e.target.result; // Assigns converted image to image object
+    //     // gImage.posIndex = i;
+    //     gImage.onload = function (ev) {
+    //         hiddenCanvas.width = gImage.width; // Assigns image's width to canvas
+    //         hiddenCanvas.height = gImage.height; // Assigns image's height to canvas
+    //     };
+    // };
 
 
 }
+
+function draw(tiles, images, ground) {
+    var tileW = 16;
+    var tileH = 16;
+    for (var y = 0; y < ground.length; y++) {
+        for (var x = 0; x < ground[y].length; x++) {
+            var tile = tiles.indexOf(ground[y][x] + "");
+            ctx.drawImage(images[tile], x * tileW, y * tileH);
+        }
+    }
+}
+
 // draw images asyncli
-const asdfg = async (imagesToDraw) => {
+const asdfg = async (imagesToDraw, seq) => {
     // console.log(imagesToDraw)
-    let hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
-    let ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
-    await obj.drawImages(ctx, imagesToDraw);
+    // let hiddenCanvas = document.getElementById("hiddenCanvas"); // Creates a canvas object
+    // let ctx = hiddenCanvas.getContext("2d"); // Creates a contect object
+
+    promiseOfAllImages(imagesToDraw)
+        .then(function (allImages) {
+            console.log("All images are loaded!", allImages); // [Img, Img, Img]
+            // draw(tiles, allImages, ground);
+            return allImages
+        }).then(all => {
+            return promiseOfAllCanvas(seq, all)
+        }).then(x=>{
+            console.log("seq is done",x)
+        }).catch(err => console.log(err))
+
+    // await drawImages(seq, imagesToDraw);
     // fill a green rect over to show we are able to await it
     console.log("all done");
 }
@@ -204,7 +358,7 @@ function ggenerator() {
     let row = []
     // let generateArray = allPossibleCombinations(numbers)[0].map
     generationNumber = Number(document.querySelector('.limit-number').value) || 9999999999999999999999999999999
-    let finalGenerationNumber = generationNumber > combinations.length ? combinations.length : generationNumber
+    finalGenerationNumber = generationNumber > combinations.length ? combinations.length : generationNumber
     // console.log(generationNumber, combinations.length,finalGenerationNumber )
     // console.log(order)
     revOrder = [...order].reverse()
@@ -220,11 +374,7 @@ function ggenerator() {
     // console.log(generateArray)
     let k = 0;
     for (k = 0; k < finalGenerationNumber; k++) {
-        putImagesToCanvas([...generateArray[k]], asdfg)
-        if (k === finalGenerationNumber - 1) {
-            downloadButton.disabled = false
-            document.querySelector('.completed').style.display = "block"
-        }
+        putImagesToCanvas([...generateArray[k]], asdfg, k)
     }
 
 }
@@ -246,7 +396,7 @@ generateButton.addEventListener('click', (e) => {
 })
 
 
-
+//seperate this
 function queryDownloader(dataURIs) {
     var zip = new JSZip();
     for (let i = 0; i < dataURIs.length; i++) {
